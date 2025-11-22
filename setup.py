a5d359cdf2e8bb90d21bad6e3d72be62a8dd53d9
Hecheel De La Cruz <lagloriaesdeyahweh@gmail.com>
2025-11-22 13:04:34 -0400

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hecheelReleases",                         # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Hecheel de la cruz",                    # Tu nombre
+    author_email="lagloriaesdeyahweh@gmail.com",    # Tu correo electrónico
+    url="https://github.com/hecheel19/hecheelReleases", # URL del proyecto
+)
